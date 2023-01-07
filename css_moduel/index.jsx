276c5b490abcac9d54c36efe3c89eee06b13e108
Kareem Elsayed <kelsayed.c@saip.gov.sa>
2023-01-07 10:20:17 +0200

    first commit

---
+++ css_moduel/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import css from './index.css'
+
+function data() {
+
+    return (
+        <>
+            <section className="${css.className} ${css.otherClassName} ${css.css_1} ${css.display} ">
+                
+            </section>
+        </>
+    )
+}
+
+
---
+++ rtl_ltr/index.md
@@ -0,0 +1,16 @@
+Logical Property css
+--------------------
+margin-left  | margin-inline-start
+
+margin-right | margin-inline-end
+
+padding-left | padding-inline-start
+
+border-right | border-inline-end
+
+left | inset-inline-start
+
+right | inset-inline-end
+
+top   | block 
+bottom  end
